430d5104e509d25c9b4e600b4067ba0ff9d0db2e
zhong_br <zph1178395080@gmail.com>
2019-05-20 22:49:49 +0800

    UUIA Python3版 SDK 将SDK封装为python包,优化调用方式

--- app.py
+++ app.py
@@ -1,43 +1,71 @@
-from UUIA import Uuia
-from hbut_data_sipder.hbut_api import *
+from UUIA import UUIA  # 导入Uuia类
 
+# Uuia类实例化
 uuia = Uuia(
     app_id="this_is_your_app_id",
     app_token="this_is_your_app_token",
     app_name="this_is_your_app_name",
     running_port=443,  # 运行端口
-    running_ip="127.0.0.1",  # 运行Ip
+    running_ip="127.0.0.1",  # 运行ip
     thread_name=__name__,  # 监听线程
     running_domain="/uuia"  # 运行url
 )
 
 
-# 绑定获取需绑定账户的函数
-@uuia.bind_action_callback_function(groups=["base", "base1"], actions=["bindType"])
+# 使用uuia实例的bind_action_callback_funcion并传入groups和actions注册相应动作的回调函数
+# 基础功能1：获取需绑定账户的回调函数
+@uuia.bind_action_callback_function(groups=["base"], actions=["bindType"])
 def get_bind_types(uuid, request_args):
-    return {
-        "uuid": uuid,
-        "accounts": [
-            {"account1": "001"},
-            {"account2": "002"},
-            {"account3": "003"},
-            {"account4": "004"},
-        ]
-    }
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
 
 
-# 绑定查询成绩的函数
+# 基础功能2：绑定账户的回调函数
+@uuia.bind_action_callback_function(groups=["base"], actions=["bind"])
+def bind_account(uuid, request_args):
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
+
+
+# 基础功能3：获取用户成绩的回调函数
 @uuia.bind_action_callback_function(groups=["base"], actions=["score"])
 def get_score(uuid, request_args):
-    return score_API(uuid)
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
 
 
-# 绑定查询课表的函数
+# 基础功能4：查询课表的回调函数
 @uuia.bind_action_callback_function(groups=["base"], actions=["schedule"])
 def get_schedule(uuid, request_args):
-    return schedule_API(uuid)
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
+
+
+# 基础功能5：获取用户信息的回调函数
+@uuia.bind_action_callback_function(groups=["base"], actions=["userInfo"])
+def get_user_info(uuid, request_args):
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
+
+
+# 基础功能6：获取用户一卡通信息的回调函数
+@uuia.bind_action_callback_function(groups=["base"], actions=["eCard"])
+def get_campus_card(uuid, request_args):
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
+
+
+# 基础功能7：获取用户考试安排的回调函数
+@uuia.bind_action_callback_function(groups=["base"], actions=["exam"])
+def get_exams_arrangements(uuid, request_args):
+    pass
+    # TODO :请在这里完成数据获取操作，并将结果以dict返回
+
+
+# 如果您还有其他功能的回调函数，按照上面的格式，根据group和action注册实现功能的回调函数即可
 
 if __name__ == '__main__':
+    # 调用uuia的run方法，启动项目，并开启flask_debug
     uuia.run(
         flask_debug=True
     )
